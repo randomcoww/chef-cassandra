0100862a579325b4a31a2aa0019571f058fb67a1
randomcoww <etherblimp@gmail.com>
2017-04-09 14:36:22 -0700

    manually generate config

--- libraries/resource/config.rb
+++ libraries/resource/config.rb
@@ -1,6 +1,8 @@
 class ChefCassandra
   class Resource
     class Config < Chef::Resource
+      include Cassandra
+
       resource_name :cassandra_config
 
       default_action :create
@@ -8,10 +10,15 @@ class ChefCassandra
 
       property :exists, [TrueClass, FalseClass]
       property :config, Hash
-      property :content, String, default: lazy { config.to_yaml }
+      property :content, String, default: lazy { to_conf }
       property :path, String, desired_state: false,
                               default: lazy { Cassandra::CONFIG_PATH }
 
+      private
+
+      def to_conf
+        generate_config(config)
+      end
     end
   end
 end
