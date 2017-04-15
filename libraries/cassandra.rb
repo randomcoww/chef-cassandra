9a6e4aae52f35b5435563bde7977f22a994fef8b
randomcoww <etherblimp@gmail.com>
2017-04-14 23:56:00 -0700

    add cqlsh wrapper

--- libraries/cassandra.rb
+++ libraries/cassandra.rb
@@ -1,3 +1,9 @@
 module Cassandra
   CONFIG_PATH ||= '/etc/cassandra/cassandra.yaml'
+
+  def cqlsh(*args)
+    command = ['cqlsh', args].compact.join(' ')
+    Chef::Log.info("running #{comamnd}")
+    shell_out!(command)
+  end
 end
